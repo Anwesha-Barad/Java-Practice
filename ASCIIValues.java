d3bb62dc8cce58b60fddced4370785ce3107792d
Anwesha Barad <anweshabarad8@gmail.com>
2026-03-14 23:56:26 +0530

    added ASCII Values

---
+++ ASCIIValues.java
@@ -0,0 +1,8 @@
+class ASCIIValues {
+    public static void main(String[] args) {
+
+        for(int i = 65; i <= 90; i++){
+            System.out.println((char)i + " = " + i);
+        }
+    }
+}
