43f79cef61d0b33c01ab1350236730c9d297bc21
Anwesha Barad <anweshabarad8@gmail.com>
2026-03-21 23:58:11 +0530

    Updated Prime Number program with optimized logic

--- Prime_no.java
+++ Prime_no.java
@@ -1,25 +1,33 @@
-//a number is a prime number or not
 import java.util.Scanner;
-class Prime_no{
-    public static void main (String[] s){
-    Scanner sc=new Scanner(System.in);
-    System.out.println("enter no:");
-    int n=sc.nextInt();
-    boolean prime=true;
 
-    if(n<=1){
-        prime= false;
-    }
-    for(int i=2;i<n;i++){
-        if(n%i==0){
-            prime=false;
+class Prime_no {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        boolean prime = true;
+
+        if (n <= 1) {
+            prime = false;
+        } 
+        else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+
+                if (n % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
         }
+
+        if (prime)
+            System.out.println(n + " is Prime Number");
+        else
+            System.out.println(n + " is Not Prime Number");
+
+        sc.close();
     }
-    if(prime){
-        System.out.println(n+"  is a prime number");
-    }
-    else{
-        System.out.println(n+"  is not a prime number");
-    }
-  }
 }
