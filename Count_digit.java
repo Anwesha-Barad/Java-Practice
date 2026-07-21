bcf57551d5f9a8b5ff3efcb3940fb04a9796776b
Anwesha Barad <anweshabarad8@gmail.com>
2026-07-21 23:52:06 +0530

    Fix digit counting variable and output

--- Count_digit.java
+++ Count_digit.java
@@ -1,16 +1,15 @@
 //count no of digit present
 import java.util.Scanner;
-
 class Count_digit{
     public static void main(String s[]){
         Scanner sc=new Scanner(System.in);
         System.out.print("enter no:");
         int n= sc.nextInt();
-        int count=0;
+        int countno=0;
         while(n>0){
             n=n/10;
-            count++;
+            countno++;
         }
-        System.out.println(count);
+        System.out.println(countno);
     }
 }
